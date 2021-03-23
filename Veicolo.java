4fbcc79dd19b8bfd1ba3538a14e9209091b454e7
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2021-03-23 10:47:11 +0100

    Update Veicolo.java

--- Veicolo.java
+++ Veicolo.java
@@ -3,37 +3,88 @@ public abstract class Veicolo {
 	
 	private int freno;
 	private int acceleratore;
+	private String marca;
+	private String modello;
 	
-	//costruttore
+	/* Inizio costruttore */
 	
-	public Veicolo(int freno, int acceleratore) {
+	public Veicolo(int freno, int acceleratore, String marca, String modello) {
 		super();
 		this.freno = freno;
 		this.acceleratore = acceleratore;
+		this.marca = marca;
+		this.modello = modello;
 	}
 
-
-
+	/* Fine Costruttore */
+	
+	/* Metodi getter e setter */
+	
 	public int getFreno() {
 		return freno;
 	}
+
+
+
+
 
 	public void setFreno(int freno) {
 		this.freno = freno;
 	}
 
+
+
+
+
 	public int getAcceleratore() {
 		return acceleratore;
 	}
+
+
+
+
 
 	public void setAcceleratore(int acceleratore) {
 		this.acceleratore = acceleratore;
 	}
 
-	@Override
-	public String toString() {
-		return "Veicolo = " + freno + " " + acceleratore;
+
+
+
+
+	public String getMarca() {
+		return marca;
 	}
+
+
+
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+
+
+
+	public String getModello() {
+		return modello;
+	}
+
+
+
+
+
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+
+
+	/* Fine metodi getter e setter */
+
+	
+	/* Metodo per la partenza */
 	
 	public void Partenza() {
 		
@@ -43,9 +94,5 @@ public abstract class Veicolo {
 			
 		}
 	}
-	
-	
-	
-
 }
 
