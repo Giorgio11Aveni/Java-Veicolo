266346114be2bd612bd200b380546c94886482af
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2021-03-27 21:00:38 +0100

    Update TestVeicolo.java

--- TestVeicolo.java
+++ TestVeicolo.java
@@ -3,7 +3,7 @@ public class TestVeicolo {
 
 	public static void main(String args[]) {
 		
-		Auto auto1 = new Auto(0,0, "Fiat", "Panda", 0);
+		Auto auto1 = new Auto(0,0, "Fiat", "Panda", 0, "Folle");
 		
 		System.out.println("Auto: " + auto1.getMarca() + " " + auto1.getModello() + "\n");
 		
